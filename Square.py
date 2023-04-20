a7d96d0aa4c9fdb0243b64a23a9bb8cbcdb1e990
Brennan Packard <02brennan.packard@gulllakecs.org>
2023-04-20 13:42:02 -0400

    Flight Forward - Stable

--- Square.py
+++ Square.py
@@ -44,12 +44,12 @@ recvThread.start()
 # CREATE FUNCTIONS HERE....
 
 
-print("\nFirst & Last Names")
-print("Program Name: ")
-print("Date: ")
+print("\nBrennan Packard")
+print("Program Name:Square")
+print("Date:4/18/23")
 print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
 print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
-ready = input('\nAre you ready to take flight: ')
+ready = input('\nAre you ready to take flight:')
 
 
 try:
@@ -57,10 +57,10 @@ try:
         print("\nStarting Drone!\n")
 
         sendmsg('command', 0)
-        sendmsg('takeoff')
+        sendmsg('takeoff', 8)
 
-        # Review the (SDK) Software Development Kit resource for Drone Commands
-        # Delete these comments before writing your program
+        sendmsg('forward 100')
+        
 
         sendmsg('land')
 
